38967e15b4d97b2fe20f50e6692d3c667a8613fa
shadman-a <shadman@outlook.com>
2020-11-16 19:38:05 -0500

    Done

--- src/dev/shadmanahmed/Main.java
+++ src/dev/shadmanahmed/Main.java
@@ -3,6 +3,10 @@ package dev.shadmanahmed;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte byte1 = 1;
+        short short1 = 2;
+        int int1 = 1000;
+        long total = (50000 + 10*(byte1 + short1 + int1));
+        System.out.println(total);
     }
 }
